0bf08fbf81beaed908ec1479d4850391a9080a8a
ArshvirGoraya <arshvir.s.goraya@gmail.com>
2023-07-14 22:38:51 -0400

    escape menu now handles mouse mode instead of player_character

--- player_character/player_character.gd
+++ player_character/player_character.gd
@@ -7,6 +7,7 @@ var mouse_sensitivity = 0.5
 @onready var synchronizer = $MultiplayerSynchronizer
 
 func _ready():
+	States.set_to_in_game()
 	synchronizer.set_multiplayer_authority(str(name).to_int())
 	camera.current = synchronizer.is_multiplayer_authority()
 	if synchronizer.is_multiplayer_authority(): Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
@@ -27,11 +28,16 @@ func _physics_process(_delta):
 		synchronizer.position = global_position
 
 func _input(event):
-#	if synchronizer.is_multiplayer_authority():
+	if synchronizer.is_multiplayer_authority():
 #		if event is InputEventKey and event.is_pressed() and event.keycode == KEY_ESCAPE:
 #			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED if Input.mouse_mode == Input.MOUSE_MODE_VISIBLE else Input.MOUSE_MODE_VISIBLE
-#		if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
-#			rotate_y(-deg_to_rad(event.relative.x) * mouse_sensitivity)
-#			synchronizer.y_rotation = rotation.y
-#			camera.rotate_x(-deg_to_rad(event.relative.y) * mouse_sensitivity)
+		if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+			rotate_y(-deg_to_rad(event.relative.x) * mouse_sensitivity)
+			synchronizer.y_rotation = rotation.y
+			camera.rotate_x(-deg_to_rad(event.relative.y) * mouse_sensitivity)
 	pass
+
+
+
+
+
