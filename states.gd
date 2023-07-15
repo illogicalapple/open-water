206b8142182b55d29fdb176dbb43e62470689222
ArshvirGoraya <arshvir.s.goraya@gmail.com>
2023-07-15 12:41:40 -0400

    added logic for settings exit and enter events

--- states.gd
+++ states.gd
@@ -1,9 +1,13 @@
 extends Node
 
+# First-level State:
 enum GameStates {MAIN_MENU, ESCAPE_MENU, IN_GAME, SETTINGS}
 
+# Second-level States:
 enum MainMenuStates {NONE, CURRENT, CHOOSE_WORLDS, SETTINGS}
 enum EscapeMenuStates {NONE, CURRENT, SETTINGS}
+
+# Third-level States: 
 enum SettingsMenuStates {NONE, CURRENT, VIDEO, AUDIO, KEY_MAPPING}
 
 var game_state := GameStates.MAIN_MENU:
@@ -32,6 +36,9 @@ func _ready() -> void:
 	main_menu_state = MainMenuStates.CURRENT
 	escape_menu_state = EscapeMenuStates.NONE
 	settings_menu_state = SettingsMenuStates.NONE
+	
+	
+	set_to_previous_menu()
 
 ## Useful for when debugging and loading straigth into an in-game scene.
 ## Should be called on the ready functions of any non-menu scenes.
@@ -40,6 +47,55 @@ func set_to_in_game() -> void:
 	escape_menu_state = EscapeMenuStates.NONE
 	main_menu_state = MainMenuStates.NONE
 	settings_menu_state = SettingsMenuStates.NONE
+
+## Sets to previous state or prints errors if not previous state is available.
+func set_to_previous_menu() -> void:
+	# Should check First Level State first, then Second Level, then Third-level, etc.
+	
+	# Get current menu:
+	match game_state:
+		GameStates.IN_GAME:
+			printerr(
+				"set_to_previous_menu(): but there is no previous menu: GameStates: ", 
+				state_str(GameStates, GameStates.IN_GAME)
+				)
+		GameStates.MAIN_MENU:
+			match main_menu_state:
+				MainMenuStates.CHOOSE_WORLDS:
+					pass
+				MainMenuStates.SETTINGS:
+					pass
+				MainMenuStates.NONE:
+					printerr("set_to_previous_menu(): GameStates in MAIN_MENU, but MainMenuState is NONE.")
+				MainMenuStates.CURRENT:
+					printerr(
+						"set_to_previous_menu(): but there is no previous menu: MainMenuStates: ",
+						state_str(MainMenuStates, MainMenuStates.CURRENT)
+						)
+
+
+## First-level State:
+#enum GameStates {MAIN_MENU, ESCAPE_MENU, IN_GAME, SETTINGS}
+#
+## Second-level States:
+#enum MainMenuStates {NONE, CURRENT, CHOOSE_WORLDS, SETTINGS}
+#enum EscapeMenuStates {NONE, CURRENT, SETTINGS}
+#
+## Third-level States: 
+#enum SettingsMenuStates {NONE, CURRENT, VIDEO, AUDIO, KEY_MAPPING}
+
+
+## Returns the enum value as String.
+## Can be used with any enum type and value due to dynamic paramaters.
+func state_str(enum_type, enum_value) -> String:
+	return enum_type.keys()[enum_value]
+
+
+func in_game() -> bool:
+	if game_state == GameStates.IN_GAME:
+		return true
+	else:
+		return false
 
 
 func in_escape_menu() -> bool:
@@ -54,4 +110,20 @@ func in_main_menu() -> bool:
 		return true
 	else:
 		return false
+
+
+func in_settings_menu() -> bool:
+	if game_state == GameStates.SETTINGS:
+		return true
+	else:
+		return false
+
+
+func in_settings_sub_menu() -> bool:
+	if (settings_menu_state == SettingsMenuStates.NONE 
+		or settings_menu_state == SettingsMenuStates.CURRENT):
+			return false
+	else:
+		return true
+
 
