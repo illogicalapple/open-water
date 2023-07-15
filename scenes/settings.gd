8f5a758eb2a6c5e698fb5492a778bf118e3a5192
ArshvirGoraya <113562877+ArshvirGoraya@users.noreply.github.com>
2023-07-15 16:12:47 -0400

    saving and loading functions set up

--- scenes/settings.gd
+++ scenes/settings.gd
@@ -1,11 +1,16 @@
 extends CanvasLayer
 
 ## Works with the States autoload to manage the current state of the SettingsMenu.
+## Currentl also saves/loads settings variables. 
+## May put loading/saving into another class instead in the future.
 
 enum VideoSettings {FOV}
 
-## Stores all settings from below.
-var settings : Dictionary
+# Using JSON:
+# From Documentation: JSON enables all data types to be converted to and from a JSON string.
+var json = JSON.new()
+
+var settings_file_path : String = "user://settings.json"
 
 var video_settings : Dictionary = {
 	VideoSettings.FOV: 75,
@@ -15,18 +20,74 @@ var audio_settings : Dictionary
 
 var key_map_settings : Dictionary
 
-var settings_file_path : String = "user://settings.cfg"
+## Stores all settings from below.
+## Is the variant that is serialized into settings.json.
+var settings : Dictionary = {
+	"vide_settings" : video_settings,
+	"audio_settings" : audio_settings,
+	"key_map_settings": key_map_settings,
+}
 
-var config = ConfigFile.new()
+func save_settings() -> void:
+	# Open settings file with write access.
+	# If file doesn't exist, should create it.
+	var settings_file := FileAccess.open(settings_file_path, FileAccess.WRITE)
+	
+	# Check if there are any errors for opening the file.
+	if settings_file.get_open_error() != Error.OK:
+		# Prints error and returns.
+		printerr("Unable to save settings: ", settings_file.get_open_error())
+		return
+	
+	# Convert settings Dictionary into JSON formatted String.
+	var settings_json_stringified : String = JSON.stringify(settings)
+	
+	# Store JSON String to file.
+	settings_file.store_string(settings_json_stringified)
+	
+	# Close File.
+	#settings_file.close() # Can remove this line. Closes by itself anyway.
 
+## Loads file, parses its content through JSON and sets it to settings Dicrionary.
+## Prints errors if unable to open or parse.
+func load_settings() -> void:
+	if FileAccess.file_exists(settings_file_path):
+		# Opens existing file
+		var settings_file := FileAccess.open(settings_file_path, FileAccess.READ)
+		
+		# Check if there are any errors for opening the file.
+		if settings_file.get_open_error() != Error.OK:
+			# Prints error and returns.
+			printerr("Unable to load settings: ", settings_file.get_open_error())
+			return
+		
+		# Get JSON String from file.
+		# Is saved as JSON String so we know it will be able to be parsed by JSON.
+		var settings_json_stringified : String = settings_file.get_as_text()
+		
+		# Close File. Have the data from it now, so don't need it anymore.
+		settings_file.close() # Can remove this line. Closes by itself anyway.
+		
+		# Convert settings Dictionary into JSON formatted String.
+		# parse_string() returns errors only.
+		var parsed_settings_result = json.parse_string(settings_json_stringified)
+		
+		# Check if there are any errors with parsing the text:
+		if parsed_settings_result == null:
+			# Prints error and returns.
+			printerr("Loaded settings, but unable to parse: ", parsed_settings_result)
+			return
+		
+		# At this point, know we can parse, so can get the data and store it in out variable.
+		settings = json.get_data()
+	else:
+		print("Settings file not found. This is okay if you haven't saved setttings yet.")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	visible = false
-	pass
-
-func save_settings() -> void:
-	pass
+	load_settings()
+	
 
 
 func exit_settings_menu() -> void:
