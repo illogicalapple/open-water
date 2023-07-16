5b06cb68798c0b20412dbe42109f682f6b981ff9
ArshvirGoraya <113562877+ArshvirGoraya@users.noreply.github.com>
2023-07-16 18:22:51 -0400

    More error handling. Data restructure. Adds new settings when found.
    Error handling: prints errors instead of crashing when a setting node cannot be
    found.
    
    Changed key_paths (in sub menu) into dictionary for better data structure.
    
    New Settings can be placed into the Settings dictionary when found.

--- scenes/VideoSubMenu.gd
+++ scenes/VideoSubMenu.gd
@@ -1,34 +1,62 @@
 extends ScrollContainer
 
-@onready var fov_slider_label : Label = $VBoxContainer/FOV/HBoxVontainer/Value
+@export_group("Labels")
+@export var fov_slider_label : Label
+@export var mouse_sensitivity_label : Label
 
+@export_group("Property Nodes")
 @export var fov_slider : HSlider
+@export var mouse_sensitivity_slider : HSlider
 
 # Storing save data with 3 values for now.
 # 1. Key for its dictionary (e.g., "FOV")
 # 2. NodePath (must be absolute)
 # 3. PropertyPath.
 # This is going to get really long...
-@onready var setting_keys : Array = [
-	["FOV", 
-	fov_slider.get_path(),
-	"value",
-	],
-]
-	
+#@onready var key_paths : Array = [
+#	["FOV", # Unique key
+#	fov_slider.get_path(), # Path to node with the property in it.
+#	"value", # Property on that node.
+#	],
+#	["MOUSE_SENSITIVITY", 
+#	mouse_sensitivity_slider.get_path(),
+#	"value",
+#	],
+#]
+@onready var key_paths : Dictionary = {
+	"FOV": # Unique key
+		[fov_slider.get_path(), # Path to node with the property in it.
+		"value" # Property on that node.
+		],
+	"MOUSE_SENSITIVITY": [mouse_sensitivity_slider.get_path(), "value"],
+}
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	fov_slider_label.text = str(fov_slider.value) 
+	fov_slider_label.text = str(fov_slider.value)
+	mouse_sensitivity_label.text = str(mouse_sensitivity_slider.value)
 
+# Saves any settings from any slider:
+func slider_change(value: float, key : String) -> void:
+	var key_array = key_paths[key]
+	var node_path = key_array[0]
+	var property_path = key_array[1]
+	
+	var save_value = [node_path, property_path, value]
+	Settings.settings["video_settings"] [key] = save_value
+
+# For any setting:
+func set_to_default(key : String) -> void:
+	Settings.default_single_video_setting(key)
+
+# For any setting:
+func reset(key : String) -> void:
+	Settings.reset_single_video_setting(key)
+
+
+# For specific settings:
 func fov_slider_change(value: float) -> void:
 	fov_slider_label.text = str(value)
-	var save_value = [fov_slider.get_path(), "value", value]
-	Settings.settings["video_settings"] ["FOV"] = save_value
 
-func set_to_default(key : String) -> void:
-	Settings.default_single_video_setting("FOV")
-
-func reset(key : String) -> void:
-	Settings.reset_single_video_setting("FOV")
-
-
+func mosue_sensitivity_slider_change(value: float) -> void:
+	mouse_sensitivity_label.text = str(value)
