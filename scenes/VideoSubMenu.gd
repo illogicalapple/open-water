a273307906edb1c905699b5381150cc230a6abec
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2023-07-16 08:25:53 -0500

    bugfix
    the fov slider path was wrong

--- scenes/VideoSubMenu.gd
+++ scenes/VideoSubMenu.gd
@@ -1,14 +1,15 @@
 extends ScrollContainer
 
-@onready var fov_slider_label : Label = $VBoxContainer/FOV/HBoxVontainer/Value
-@onready var fov_slider : HSlider = $VBoxContainer/FOV/HBoxVontainer/HSlider
+@onready var fov_slider_label : Label = $VBoxContainer/FOV/Value
+@onready var fov_slider : HSlider = $VBoxContainer/FOV/HSlider
 
-@onready var setting_keys : Array = [
-	[fov_slider.get_path(), "value"],
-]
+@onready var setting_keys : Array
 	
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	setting_keys = [
+		[fov_slider.get_path(), "value"],
+	]
 	fov_slider_label.text = str(fov_slider.value) 
 
 func fov_slider_change(value: float) -> void:
