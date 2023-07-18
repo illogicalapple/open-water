5170f0ea0a3b180ec6f99d17fa985361ceb57b17
R9_ <neamtrares@naver.com>
2023-07-18 20:15:15 +0300

    Connect to server with custom IP adress

--- main.gd
+++ main.gd
@@ -7,18 +7,19 @@ var time: Dictionary = {
 }
 var multiplayer_peer: ENetMultiplayerPeer = ENetMultiplayerPeer.new()
 
-@onready var menu: VBoxContainer = $Menu
+@onready var menu: Control = $MenuControl
 
 # join multiplayer room
 func _on_join_pressed():
-	var port = str($Menu/Port.text).to_int()
-	multiplayer_peer.create_client("localhost", port) # always localhost, must change that
+	var port = str($MenuControl/Menu/Port.text).to_int()
+	var ip = str($MenuControl/Menu/IP.text)
+	multiplayer_peer.create_client(ip, port)
 	multiplayer.multiplayer_peer = multiplayer_peer
 	menu.visible = false
 
 # multiplayer host
 func _on_host_pressed():
-	var port = str($Menu/Port.text).to_int()
+	var port = str($MenuControl/Menu/Port.text).to_int()
 	multiplayer_peer.create_server(port)
 	multiplayer.multiplayer_peer = multiplayer_peer
 	multiplayer_peer.peer_connected.connect(func(id): add_player_character(id))
