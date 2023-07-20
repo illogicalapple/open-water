fea1cf26b18eaf2e3bf177508b0f6e4b5126d3d7
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2023-07-20 08:58:21 -0500

    move some stuff around

---
+++ scenes/components/SettingsSubMenuSelector.gd
@@ -0,0 +1,50 @@
+extends ScrollContainer
+
+# Hides/Shows the correct sub menus.
+@export var current_state := States.SettingsMenuStates.CONTROLS
+
+@export var video_submenu : VideoSubMenu
+@export var key_map_submenu : ControlsSubMenu
+
+func _ready() -> void:
+	pass
+
+# Called when settings is entered.
+func pick_current_state() -> void:
+	match current_state:
+		States.SettingsMenuStates.CONTROLS:
+			_on_controls_pressed()
+		States.SettingsMenuStates.VIDEO:
+			_on_video_pressed()
+		_:
+			# If anything else is selected just go to another menu.
+			current_state = States.SettingsMenuStates.CONTROLS
+			_on_controls_pressed()
+
+func _on_video_pressed() -> void:
+	current_state = States.SettingsMenuStates.VIDEO
+	States.settings_menu_state = current_state
+	video_submenu.show()
+	key_map_submenu.hide()
+	
+
+func _on_controls_pressed() -> void:
+	current_state = States.SettingsMenuStates.CONTROLS
+	States.settings_menu_state = current_state
+	key_map_submenu.show()
+	video_submenu.hide()
+	
+#func _on_key_mapping_pressed() -> void:
+#	current_state = States.SettingsMenuStates.KEY_MAPPING
+#	States.settings_menu_state = current_state
+#	key_map_submenu.show()
+#	video_submenu.hide()
+	
+
+func _on_sounds_pressed() -> void:
+	print ("no sound submenu yet.")
+
+
+
+
+
