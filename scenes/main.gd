0c9769e509d7912db03a985622df36c83cdff0db
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2023-07-23 08:23:24 -0500

    moar things (!!!)

--- scenes/main.gd
+++ scenes/main.gd
@@ -19,22 +19,27 @@ var multiplayer_peer: ENetMultiplayerPeer = ENetMultiplayerPeer.new()
 
 @onready var menu: Control = $MenuControl
 
+func _ready():
+	$GUI.hide()
+
 # join multiplayer room
 func _on_join_pressed():
-	var port = str($MenuControl/Menu/Port.text).to_int()
-	var ip = str($MenuControl/Menu/IP.text)
+	var port = str($MenuControl/Menu/IP/Port.text).to_int()
+	var ip = str($MenuControl/Menu/IP/IP.text)
 	multiplayer_peer.create_client(ip, port)
 	multiplayer.multiplayer_peer = multiplayer_peer
 	menu.visible = false
+	$GUI.show()
 
 # multiplayer host
 func _on_host_pressed():
-	var port = str($MenuControl/Menu/Port.text).to_int()
+	var port = str($MenuControl/Menu/IP/Port.text).to_int()
 	multiplayer_peer.create_server(port)
 	multiplayer.multiplayer_peer = multiplayer_peer
 	multiplayer_peer.peer_connected.connect(func(id): add_player_character(id))
 	menu.visible = false
 	add_player_character()
+	$GUI.show()
 
 # adds a character
 func add_player_character(id=1):
