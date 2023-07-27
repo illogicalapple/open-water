0a5e0f3bd7b850c477e326d1f1744a314ada1a2d
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2023-07-26 20:32:13 -0500

    some stuff?
    smh

--- scenes/main.gd
+++ scenes/main.gd
@@ -9,12 +9,7 @@ var is_underwater: bool = false:
 		else:
 			$Underwater.hide()
 			($WorldEnvironment.environment as Environment).fog_density = 0.004
-			
-var time: Dictionary = {
-	"hour": 7,
-	"minute": 0,
-	"am": true
-}
+
 var multiplayer_peer: ENetMultiplayerPeer = ENetMultiplayerPeer.new()
 
 @onready var menu: Control = $MenuControl
