d1470161df0a9202347da0255bf8520805d78cb7
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2023-07-30 09:48:29 -0500

    added tooltips

--- scenes/components/inventory_item.gd
+++ scenes/components/inventory_item.gd
@@ -13,8 +13,10 @@ extends TextureButton
 var hotbar_texture: CompressedTexture2D = preload("res://assets/images/inventory/hotbar_square.png")
 var hotbar_active_texture: CompressedTexture2D = preload("res://assets/images/inventory/selected.png")
 var item: InventoryItem = InventoryItem.new()
+var config_file := ConfigFile.new()
 
 func _ready():
+	config_file.load("res://behavior/items.cfg")
 	if is_hotbar:
 		texture_normal = hotbar_texture
 
@@ -27,11 +29,28 @@ func _process(_delta):
 	if item.type == "air":
 		$TextureRect.hide()
 		$Amount.hide()
+		$Tooltip.hide()
 	else:
+		var color: String = "#ffffff"
 		$TextureRect.show()
 		$Amount.show()
+		match config_file.get_value(item.type, "rarity", 1):
+			1:
+				color = "#ffffff"
+			2:
+				color = "#ffc336"
+			3:
+				color = "#f0abff"
+		
+		$Tooltip.text = "[color=%s][b]%s[/b][/color]\n[color=#b8b8b8]%s[/color]\n\n%s" % [color, config_file.get_value(item.type, "name", item.type), config_file.get_value(item.type, "type", "useless"), config_file.get_value(item.type, "tooltip", "no description")]
+		if get_global_rect().has_point(get_global_mouse_position()):
+			$Tooltip.show()
+			$Tooltip.global_position = get_global_mouse_position()
+		else:
+			$Tooltip.hide()
 
 
 func _on_pressed():
 	States.inventory_item = index
 	print(States.inventory_item)
+
