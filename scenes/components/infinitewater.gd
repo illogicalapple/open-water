09a28da22d26840bbdf7e61dc25de263b69f5cb2
m125 <kaman.marcell.2009.10.28@gmail.com>
2023-08-08 13:49:36 +0200

    forgot to use playersync group at water

--- scenes/components/infinitewater.gd
+++ scenes/components/infinitewater.gd
@@ -14,7 +14,7 @@ func _process(delta):
 	#finding player of game instance
 	if ourplayer==null:
 		for e in get_parent().get_children():
-			if e.get_node_or_null("MultiplayerSynchronizer")!=null:
+			if e.get_node_or_null("MultiplayerSynchronizer")!=null and e.get_node_or_null("MultiplayerSynchronizer").is_in_group("playersync"):
 				if e.get_node_or_null("MultiplayerSynchronizer").is_multiplayer_authority():
 					#multiplayer authority c:
 					ourplayer=e
