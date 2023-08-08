cf5b83f7099e45db0df7011aa6aa68394cdb608d
m125 <kaman.marcell.2009.10.28@gmail.com>
2023-08-08 13:47:02 +0200

    infinite mountains(without collision for now) added group playersync cuz thing are following the raft

--- scenes/terrain.gd
+++ scenes/terrain.gd
@@ -6,6 +6,10 @@ var time=0
 var noise=FastNoiseLite.new()
 var distance_per_points=20
 func gn(x,z) ->float:#shortening get_noise_2d
+#	x*=distance_per_points
+#	z*=distance_per_points
+	x+=floor(global_position.x/distance_per_points)
+	z+=floor(global_position.z/distance_per_points)
 	return noise.get_noise_2d(x,z)*20
 
 func _ready():
@@ -13,18 +17,18 @@ func _ready():
 	var vertices = PackedVector3Array()
 	var index=PackedInt32Array()
 	var usedindex=0
-	for x in range(10):
-		for z in range(10):
+	for x in range(-5,5):
+		for z in range(-5,5):
 			
 			
 			
-			vertices.push_back(Vector3(x,gn(x,z),z)*10)
-			vertices.push_back(Vector3(x+1,gn(x+1,z),z)*10)
-			vertices.push_back(Vector3(x,gn(x,z+1),z+1)*10)
+			vertices.push_back(Vector3(x,gn(x,z),z)*distance_per_points)
+			vertices.push_back(Vector3(x+1,gn(x+1,z),z)*distance_per_points)
+			vertices.push_back(Vector3(x,gn(x,z+1),z+1)*distance_per_points)
 			
-			vertices.push_back(Vector3(x+1,gn(x+1,z+1),z+1)*10)
-			vertices.push_back(Vector3(x,gn(x,z+1),z+1)*10)
-			vertices.push_back(Vector3(x+1,gn(x+1,z),z)*10)
+			vertices.push_back(Vector3(x+1,gn(x+1,z+1),z+1)*distance_per_points)
+			vertices.push_back(Vector3(x,gn(x,z+1),z+1)*distance_per_points)
+			vertices.push_back(Vector3(x+1,gn(x+1,z),z)*distance_per_points)
 			
 			
 			
@@ -54,13 +58,16 @@ func _process(delta):
 	#finding player of game instance
 	if ourplayer==null:
 		for e in get_parent().get_children():
-			if e.get_node_or_null("MultiplayerSynchronizer")!=null:
+			if e.get_node_or_null("MultiplayerSynchronizer")!=null and e.get_node_or_null("MultiplayerSynchronizer").is_in_group("playersync"):
 				if e.get_node_or_null("MultiplayerSynchronizer").is_multiplayer_authority():
 					#multiplayer authority c:
 					ourplayer=e
 	else:
+		var prevpos=position
 		var pos=floor(Vector3(ourplayer.position.x,position.y,ourplayer.position.z)/distance_per_points)*distance_per_points
 		pos.y=position.y 
 		position=pos
+		if position!=prevpos:
+			_ready()
 		#follow player on x and z coordinates
 #	self.material_override.set_shader_parameter("offset",position)
