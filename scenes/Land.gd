ae55311565d237ab0015a24e6be39b8a6adb5885
m125 <kaman.marcell.2009.10.28@gmail.com>
2023-08-20 17:01:30 +0200

    uuuuh yeah land node is not following u... TILL NOW

---
+++ scenes/Land.gd
@@ -0,0 +1,19 @@
+extends StaticBody3D
+var ourplayer=null
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if ourplayer==null:
+		for e in get_parent().get_children():
+			if e.get_node_or_null("MultiplayerSynchronizer")!=null and e.get_node_or_null("MultiplayerSynchronizer").is_in_group("playersync"):
+				if e.get_node_or_null("MultiplayerSynchronizer").is_multiplayer_authority():
+					#multiplayer authority c:
+					ourplayer=e
+	else:
+		position.x = ourplayer.position.x
+		position.z=ourplayer.position.z
